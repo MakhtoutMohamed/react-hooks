49f1141fd1117f5babe1ff56aafc9f0fa75f4620
Mohamed Makhtout <medmakhtout@gmail.com>
2025-06-24 22:41:45 +0100

    Implémenter une recherche en temps réel

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -1,19 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { ThemeContext } from '../App';
+import {useDebounce} from '../hooks/useProductSearch'
 
-const ProductSearch = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+const ProductSearch = ({ searchTerm, setSearchTerm }) => {
+  //const [searchTerm, setSearchTerm] = useState('');
   const { isDarkTheme } = useContext(ThemeContext);
-  // TODO: Exercice 2.1 - Utiliser le LanguageContext
-  
-  // TODO: Exercice 1.2 - Utiliser le hook useDebounce
+
+  const [inpVal, setInpVal] = useState(searchTerm);
+  const debouncedValue = useDebounce(inpVal, 100);
+
+  useEffect(() => {
+    setSearchTerm(debouncedValue);
+  }, [debouncedValue, setSearchTerm]);
+
   
   return (
     <div className="mb-4">
       <input
         type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        value={inpVal}
+        onChange={(e) => setInpVal(e.target.value)}
         placeholder="Rechercher un produit..."
         className={`form-control ${isDarkTheme ? 'bg-dark text-light' : ''}`}
       />
