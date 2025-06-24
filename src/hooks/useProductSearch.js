2eeab8d724426fdfdbca94711bfccb3d4ab1831c
Mohamed Makhtout <medmakhtout@gmail.com>
2025-06-24 22:53:51 +0100

    Context et Internationalisation

--- src/hooks/useProductSearch.js
+++ src/hooks/useProductSearch.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 
-// TODO: Exercice 3.1 - Créer le hook useDebounce
 export const useDebounce = (value, delay) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
@@ -13,18 +12,15 @@ export const useDebounce = (value, delay) => {
   }, [value, delay]);
   return debouncedValue;
 };
-// TODO: Exercice 3.2 - Créer le hook useLocalStorage
 
 const useProductSearch = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  // TODO: Exercice 4.2 - Ajouter l'état pour la pagination
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        // TODO: Exercice 4.2 - Modifier l'URL pour inclure les paramètres de pagination
         const response = await fetch('https://api.daaif.net/products?delay=1000');
         if (!response.ok) throw new Error('Erreur réseau');
         const data = await response.json();
@@ -37,17 +33,12 @@ const useProductSearch = () => {
     };
 
     fetchProducts();
-  }, []); // TODO: Exercice 4.2 - Ajouter les dépendances pour la pagination
-
-  // TODO: Exercice 4.1 - Ajouter la fonction de rechargement
-  // TODO: Exercice 4.2 - Ajouter les fonctions pour la pagination
+  }, []);
 
   return { 
     products, 
     loading, 
     error,
-    // TODO: Exercice 4.1 - Retourner la fonction de rechargement
-    // TODO: Exercice 4.2 - Retourner les fonctions et états de pagination
   };
 };
 
